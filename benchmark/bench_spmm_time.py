120b193c83e9985af032862cf1acd5f6011db948
xiakubaobaore <1052469954@qq.com>
2024-01-10 15:34:11 +0800

    add torch.sparse.mm benchmark

--- benchmark/bench_spmm_time.py
+++ benchmark/bench_spmm_time.py
@@ -21,6 +21,27 @@ class SpMMSum:
                                         requires_grad=True)
 
     def forward_check(self):
+
+        for _ in range(10):
+            torch.sparse.mm(self.tcsr, self.input_feature)
+        torch.cuda.synchronize()
+        start = time.time()
+        for _ in range(100):
+            torch.sparse.mm(self.tcsr, self.input_feature)
+        torch.cuda.synchronize()
+        end = time.time()
+        torch_sparse_time = end - start
+
+        for _ in range(10):
+            spmm_sum(self.dcsr, self.input_feature, -1)
+        torch.cuda.synchronize()
+        start = time.time()
+        for _ in range(100):
+            spmm_sum(self.dcsr, self.input_feature, -1)
+        torch.cuda.synchronize()
+        end = time.time()
+        rocsparse_time = end - start
+
         for _ in range(10):
             spmm_sum(self.dcsr, self.input_feature, self.algorithm)
         torch.cuda.synchronize()
@@ -30,53 +51,137 @@ class SpMMSum:
         torch.cuda.synchronize()
         end = time.time()
         dgsparse_time = end - start
-        return dgsparse_time
+        return torch_sparse_time, rocsparse_time, dgsparse_time
 
     def backward_check(self):
-        return 0
+        for _ in range(10):
+            out = torch.sparse.mm(self.tcsr, self.input_feature)
+        torch.cuda.synchronize()
+        start = time.time()
+        for _ in range(100):
+            out = torch.sparse.mm(self.tcsr, self.input_feature)
+            out.sum().backward()
+        torch.cuda.synchronize()
+        end = time.time()
+        torch_sparse_time = end - start
+
+        # temp = self.tcsr.clone().detach()
+        # for _ in range(10):
+        #     out = torch.matmul(temp, self.input_feature)
+        # torch.cuda.synchronize()
+        # start = time.time()
+        # for _ in range(100):
+        #     out = torch.matmul(temp, self.input_feature)
+        #     out.sum().backward()
+        # torch.cuda.synchronize()
+        # end = time.time()
+        # torch_matmul_time = end - start
+
+        for _ in range(10):
+            out = spmm_sum(self.dcsr, self.input_feature, self.algorithm)
+        torch.cuda.synchronize()
+        start = time.time()
+        for _ in range(100):
+            out = spmm_sum(self.dcsr, self.input_feature, self.algorithm)
+            out.sum().backward()
+        torch.cuda.synchronize()
+        end = time.time()
+        dgsparse_time = end - start
+
+        return torch_sparse_time, 0, dgsparse_time
 
 
 def check_time(gc, stage='forward'):
     print(f'{stage} time:')
+    # torch_sparse_time_list = []
+    # rocsparse_time_list = []
+    # dgsparse_time_list = []
     if stage == 'forward':
-        dgsparse_time = gc.forward_check()
+        torch_sparse_time, rocsparse_time, dgsparse_time = gc.forward_check()
     elif stage == 'backward':
-        dgsparse_time = gc.backward_check()
+        torch_sparse_time, rocsparse_time, dgsparse_time = gc.backward_check()
     else:
         raise ValueError
-    print(f'dgsparse_{gc.algorithm} {stage} time is: {dgsparse_time}')
-    return dgsparse_time
+    # torch_sparse_time_list.append(torch_sparse_time)
+    # rocsparse_time_list.append(rocsparse_time)
+    # dgsparse_time_list.append(dgsparse_time)
+    # print(f'torch_sparse {stage} time is: {torch_sparse_time_list}')
+    # print(f'rocsparse {stage} time is: {rocsparse_time_list}')
+    # print(f'dgsparse {stage} time is: {dgsparse_time_list}')
+
+    print(f'torch_sparse {stage} time is: {torch_sparse_time:.4f}')
+    print(f'rocsparse {stage} time is: {rocsparse_time:.4f}')
+    print(f'dgsparse {stage} time is: {dgsparse_time:.4f}')
 
 
-def test_spmm_time(dataset, in_dim, device, reduce='sum', algorithm=0):
+# def check_time(gc, stage='forward'):
+#     print(f'{stage} time:')
+#     if stage == 'forward':
+#         dgsparse_time = gc.forward_check()
+#     elif stage == 'backward':
+#         dgsparse_time = gc.backward_check()
+#     else:
+#         raise ValueError
+#     print(f'dgsparse_{gc.algorithm} {stage} time is: {dgsparse_time}')
+#     return dgsparse_time
+
+
+# def test_spmm_time(dataset, in_dim, device, reduce='sum', algorithm=0):
+#     print()
+#     print(
+#         f'start testing {dataset} dataset, reduce is: {reduce}, in_dim is: {in_dim}, algorithm = {algorithm}'
+#     )
+#     data = GraphDataset(dataset, device)
+#     if (reduce == 'sum'):
+#         gc = SpMMSum(data, in_dim, device, algorithm)
+#     else:
+#         raise ValueError
+#     return check_time(gc, stage='forward')
+
+def test_spmm_time(dataset, in_dim, device, reduce='sum'):
     print()
-    print(
-        f'start testing {dataset} dataset, reduce is: {reduce}, in_dim is: {in_dim}, algorithm = {algorithm}'
-    )
+    print(f'start testing {dataset} dataset, \
+        reduce is: {reduce}, in_dim is: {in_dim}')
     data = GraphDataset(dataset, device)
-    if (reduce == 'sum'):
-        gc = SpMMSum(data, in_dim, device, algorithm)
+    if reduce == 'sum':
+        gc = SpMMSum(data, in_dim, device, 0)
+    # elif reduce == 'max':
+    #     gc = SpMMMax(data, in_dim, device, 0)
+    # elif reduce == 'min':
+    #     gc = SpMMMin(data, in_dim, device, 0)
+    # elif reduce == 'mean':
+    #     gc = SpMMMean(data, in_dim, device, 0)
     else:
         raise ValueError
-    return check_time(gc, stage='forward')
+    # check_time(gc, stage='forward')
+    check_time(gc, stage='backward')
 
 
 if __name__ == '__main__':
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     datasets = ['cora', 'citeseer', 'pubmed', 'ppi']
     features_dim = [32, 64, 128]
-    algorithms = [-1, 0, 1, 2, 3]
-    all_time = []
-    for algorithm in algorithms:
-        time_list = []
-        for dataset in datasets:
-            for in_dim in features_dim:
-                time_list.append(
-                    test_spmm_time(dataset,
-                                   in_dim,
-                                   device,
-                                   reduce='sum',
-                                   algorithm=algorithm))
-        all_time.append(time_list)
-    time_result = torch.tensor(all_time)
-    print(time_result)
+    for dataset in datasets:
+        for in_dim in features_dim:
+            test_spmm_time(dataset, in_dim, device, reduce='sum')
+
+
+# if __name__ == '__main__':
+#     device = 'cuda' if torch.cuda.is_available() else 'cpu'
+#     datasets = ['cora', 'citeseer', 'pubmed', 'ppi']
+#     features_dim = [32, 64, 128]
+#     algorithms = [-1, 0, 1, 2, 3]
+#     all_time = []
+#     for algorithm in algorithms:
+#         time_list = []
+#         for dataset in datasets:
+#             for in_dim in features_dim:
+#                 time_list.append(
+#                     test_spmm_time(dataset,
+#                                    in_dim,
+#                                    device,
+#                                    reduce='sum',
+#                                    algorithm=algorithm))
+#         all_time.append(time_list)
+#     time_result = torch.tensor(all_time)
+#     print(time_result)
